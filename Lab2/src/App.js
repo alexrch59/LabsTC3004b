3f69ed96d90018fe62b2aa875c316f218aef12de
alexrch59 <a01284006@tec.mx>
2025-08-25 19:37:49 -0600

    Initial commit

---
+++ Lab2/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CounterApp from "./CounterApp";
+
+function App() {
+  return (
+    <div>
+      <CounterApp value={10} counter={5} />
+    </div>
+  );
+}
+
+export default App;
